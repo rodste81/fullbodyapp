41856d3eaf868aa8f4508302b1cb6abfde3e0403
rodste81 <130950821+rodste81@users.noreply.github.com>
2025-03-13 01:12:02 -0300

    Update db.py

--- db.py
+++ db.py
@@ -53,8 +53,7 @@ def criar_banco():
     conn.close()
 
 if __name__ == "__main__":
-   # Criar banco APENAS se não existir
-    
-if not os.path.exists("workout2.db"):
-    criar_banco()
+    # Criar banco APENAS se não existir
+    if not os.path.exists("workout2.db"):
+        criar_banco()
     print("✅ Banco de dados criado e pronto para uso!")
