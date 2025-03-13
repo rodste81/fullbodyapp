36e82734c4b034dbb90e44b4fb4eb72da169e489
rodste81 <130950821+rodste81@users.noreply.github.com>
2025-03-13 01:39:56 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,14 @@ if not os.path.exists("workout2.db"):
     criar_banco()
     print("✅ Banco de dados criado e pronto para uso!")
 
+    
+# Verifica se o usuário está logado
+if "usuario" not in st.session_state or "usuario_id" not in st.session_state:
+    login()
+    st.stop()
+
+usuario_id = st.session_state["usuario_id"]
+
 # Se o usuário for "rod", incluir o menu "Admin"
 if st.session_state["usuario"] == "rod":
     menu = st.sidebar.radio("Menu", ["Admin", "Peitorais", "Costas", "Ombro", "Biceps", "Triceps", "Pernas", "Abdomen", "Cadastrar Exercício", "Make iT!", "Histórico de Treinos", "Sair"])
@@ -18,14 +26,6 @@ else:
 # Chamar função correspondente ao menu escolhido
 if menu == "Admin" and st.session_state["usuario"] == "rod":
     carregar_admin()
-    
-# Verifica se o usuário está logado
-if "usuario" not in st.session_state or "usuario_id" not in st.session_state:
-    login()
-    st.stop()
-
-usuario_id = st.session_state["usuario_id"]
-
 
     
 # Garantindo que os menus apareçam corretamente
