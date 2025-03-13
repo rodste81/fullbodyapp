4a0651daced2079c6bacbdf9a2688167fa5a72a3
rodste81 <130950821+rodste81@users.noreply.github.com>
2025-03-13 01:26:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from auth import login
 from menus import carregar_conteudo_menu
 from db import criar_banco, get_connection
+from admin import carregar_admin  # Importa o painel de administração
 
 criar_banco()  # Garante que o banco seja criado antes de rodar o app
 
@@ -13,6 +14,16 @@ if "usuario" not in st.session_state or "usuario_id" not in st.session_state:
 
 usuario_id = st.session_state["usuario_id"]
 
+# Se o usuário for "rod", incluir o menu "Admin"
+if st.session_state["usuario"] == "rod":
+    menu = st.sidebar.radio("Menu", ["Admin", "Peitorais", "Costas", "Ombro", "Biceps", "Triceps", "Pernas", "Abdomen", "Cadastrar Exercício", "Make iT!", "Histórico de Treinos", "Sair"])
+else:
+    menu = st.sidebar.radio("Menu", ["Peitorais", "Costas", "Ombro", "Biceps", "Triceps", "Pernas", "Abdomen", "Cadastrar Exercício", "Make iT!", "Histórico de Treinos", "Sair"])
+
+# Chamar função correspondente ao menu escolhido
+if menu == "Admin" and st.session_state["usuario"] == "rod":
+    carregar_admin()
+    
 # Garantindo que os menus apareçam corretamente
 st.sidebar.title(f"👤 Usuário: {st.session_state['usuario']}")
 menu = st.sidebar.radio("Menu", ["Peitorais", "Costas", "Ombro", "Biceps", "Triceps", "Pernas", "Abdomen", "Cadastrar Exercício", "Make iT!", "Histórico de Treinos", "Sair"])
